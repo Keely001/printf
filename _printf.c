9d9c7afad8e737e26051f4bae8f585cadb9f0de9
Keely001 <thindiuke@gmail.com>
2023-04-23 14:18:53 -0700

    printf update

--- _printf.c
+++ _printf.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+
+void char_printer(va_list args, int *i);
+void str_printer(va_list args, int *i);
+void percentage_printer(int *i);
+
 /**
  * _printf - produces output according to format
- * @format: charachter string to be printed
- *
- * Return: the number of characters printed
+ * @format: pointer to a string
+ *..: variadic arguments.
+ * Return: the number of charachters printed
  */
 
 int _printf(const char *format, ...)
@@ -12,40 +17,81 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i = 0;
 
-	while (*format)
+	va_start(args, format);
+
+	while (*format != '\0')
 	{
-		if (*format == 'c')
+		if (*format == '%')
 		{
-			const char b = va_arg(args, int);
-
-			_putchar(b);
-			i++;
-		}
-		else if (*format == 's')
-		{
-			char *str = va_arg(args, char*);
-
-			if (str == NULL)
+			format++;
+			if (*format == 'c')
+				char_printer(args, &i);
+			else if (*format == 's')
+				str_printer(args, &i);
+			else if (*format == '%')
+				percentage_printer(&i);
+			else
 			{
-				str = "(null)";
-			}
-			while (*str)
-			{
-				_putchar(*str);
-				str++;
-				i++;
+				_putchar('%');
+				_putchar(*format);
+				i += 2;
 			}
 		}
 		else
-		{
-			if (*format == '%')
-			{
-				_putchar('%');
-				i++;
-			}
-		}
+			i += _putchar(*format);
 		format++;
 	}
+
 	va_end(args);
+
 	return (i);
 }
+
+/**
+ * char_printer - function to print char
+ * @args: argument
+ * @i: pointer to counter
+ * Return: none
+ */
+
+void char_printer(va_list args, int *i)
+{
+	char a = va_arg(args, int);
+
+	*i += 1;
+	_putchar(a);
+
+}
+
+/**
+ * str_printer - function to print strng
+ * @args: argument
+ * @i: pointer to counter
+ * Return: none
+ */
+
+void str_printer(va_list args, int *i)
+{
+	char *str = va_arg(args, char*);
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		*i += 1;
+		str++;
+	}
+}
+
+/**
+ * percentage_printer - function to print char
+ * @i: pointer to counter
+ * Return: none
+ */
+void percentage_printer(int *i)
+{
+	_putchar('%');
+	*i += 1;
+}
+
